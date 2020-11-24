b6cb0b7fd795199d60454f9152356ac11584f0f3
iwasatat0107 <it.was07@gmail.com>
2020-11-24 15:10:37 +0900

    createアクション

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,4 +6,13 @@ class ClientsController < ApplicationController
   def new
     @client = Client.new
   end
+
+  def create
+    Client.create(client_params)
+  end
+
+  private
+  def client_params
+    params.require(:client).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :company, :prefecture_id, :birthday, :image)
+  end
 end
