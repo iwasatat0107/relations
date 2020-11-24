a1727ee5f69ecd35c273d6788e0f28ecedbf832e
iwasatat0107 <it.was07@gmail.com>
2020-11-24 14:44:07 +0900

    userモデルとclientモデルのバリデーション

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,10 @@
 class Client < ApplicationRecord
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  belongs_to :user
 end
