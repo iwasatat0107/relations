ab8e67d7627c1969d3efe3c3b813e01a3b744222
iwasatat0107 <it.was07@gmail.com>
2020-11-29 17:16:22 +0900

    ログインかつ親ユーザーのみ許可

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,17 +1,17 @@
 class ChecksController < ApplicationController
+  before_action :set_check, only: [:index, :new, :create]
+  before_action :move_to_index, except: :index
+
   def index
     @check = Client.new
-    @client = Client.find(params[:client_id])
     @checks = @client.checks.includes(:user).order("created_at DESC")
   end
 
   def new
-    @client = Client.find(params[:client_id])
     @check = Check.new
   end
 
   def create
-    @client = Client.find(params[:client_id])
     @check = Check.new(check_params)
     if @check.valid?
       @check.save
@@ -24,6 +24,16 @@ class ChecksController < ApplicationController
   private
   def check_params
     params.require(:check).permit(:title, :smile_id, :aizuchi_id, :empathy_id, :reaction_id, :question_id).merge(user_id: current_user.id, client_id: params[:client_id])
+  end
+
+  def set_check
+    @client = Client.find(params[:client_id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @client.user_id
+      redirect_to action: :index
+    end
   end
 
 end
