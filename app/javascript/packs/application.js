a2a44efad8482aa55cf5978b6bb4d03a1040db31
iwasatat0107 <it.was07@gmail.com>
2020-12-05 11:33:16 +0900

    画像プレビューの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,10 @@
 //= require bootstrap
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('./preview')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
