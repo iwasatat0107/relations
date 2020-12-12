e937030c16e36116c1b6240ebf8d420d2631425c
iwasatat0107 <it.was07@gmail.com>
2020-12-12 16:21:53 +0900

    ビューの調整

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,22 +2,17 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
 
-     // 選択した画像を表示する関数
      const createImageHTML = (blob) => {
-     // 画像を表示するためのdiv要素を生成
      const imageElement = document.createElement('div');
 
-     // 表示する画像を生成
      const blobImage = document.createElement('img');
      blobImage.setAttribute('src', blob);
 
-     // 生成したHTMLの要素をブラウザに表示させる
     imageElement.appendChild(blobImage);
     ImageList.appendChild(imageElement);
      };
 
     document.getElementById('client_image').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
       const imageContent = document.querySelector('img');
       if (imageContent){
         imageContent.remove();
