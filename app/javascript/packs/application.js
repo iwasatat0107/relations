a28c89a70bb1edc74d0d5064445adb2b574c5264
iwasatat0107 <it.was07@gmail.com>
2020-12-13 10:31:35 +0900

    add )

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery'
+require('jquery')
 require('./preview')
 
 
